6ce6a9e4e021e7e10e4ca6f63ec70247b64c8cdf
Arthur Campbell <arfa.camble@gmail.com>
2021-03-21 10:40:47 +0000

    tiny seed change

--- db/seeds.rb
+++ db/seeds.rb
@@ -10,68 +10,6 @@ User.destroy_all
 Update.destroy_all
 Api.destroy_all
 
-# APIS
-monday = Api.new(
-  name:   "Monday API",
-  status: "pending",
-  tray_version: "2.0.1",
-  latest_version: "2.0.1",
-  developer: "unassigned",
-  description: "The monday.com API supports read and write (mutating) access to boards, items, and more. It gives you the ability to both view and use any data you have in monday.com outside of the platform. You can use the API to import external data into your monday.com account or trigger actions in monday.com in response to a change in an external software or service.",
-  api_acc_manager: "William Gates | team@monday.com | +447237300021",
-  logo_url: "https://4bo0cq4bxnou2rimh3hwzwi8-wpengine.netdna-ssl.com/blog/wp-content/uploads/2018/02/22852120_1266763086768693_6004893502123596052_n.png",
-)
-monday.save!
-
-microsoft = Api.new(
-  name:   "Microsoft 365 API",
-  status: "pending",
-  tray_version: "2.0.12",
-  latest_version: "2.0.11",
-  developer: "unassigned",
-  description: "Office 365 services, such as OneNote, Outlook, Excel, OneDrive, Microsoft Teams, Planner, and SharePoint, are now exposed in Microsoft Graph. Microsoft Graph is a unified API endpoint for accessing data across Microsoft 365, which includes Office 365, Enterprise Mobility, and Security and Windows services. It provides a simplified developer experience, with one endpoint and a single authentication token that gives your app access to data across all these services.",
-  api_acc_manager: "Miranda Gates | conections@microsoft.com | +447237300021",
-  logo_url: "https://cdn1.iconfinder.com/data/icons/flat-and-simple-part-1/128/microsoft-512.png",
-)
-microsoft.save!
-
-oxford = Api.new(
-  name:   "Oxford Dictionaries API",
-  status: "pending",
-  tray_version: "3.1.12",
-  latest_version: "2.8.9",
-  developer: "unassigned",
-  description: "The Oxford Dictionaries API allows easy access to our world-renowned dictionary content. Use the live documentation below to try it out, view real responses, and explore a growing number of code samples to help you get started.",
-  api_acc_manager: "Colin Smith | emailresponses.ac@oup.com | +447237300021",
-  logo_url: "https://pbs.twimg.com/profile_images/875679902216970241/NAw23Gdg_400x400.jpg",
-)
-oxford.save!
-
-linkedin = Api.new(
-  name:   "LinkedIn API",
-  status: "pending",
-  tray_version: "1.5.12",
-  latest_version: "1.5.8",
-  developer: "unassigned",
-  description: "LinkedIn's home for API documentation for all LinkedIn business lines. Our API documentation is organized by business lines covering Consumer, Compliance, Learning, Marketing, Sales, and Talent Solutions. Follow the links below to learn more about business lines and their possible integration types.",
-  api_acc_manager: "Rich Links | connections@microsoft.com | +447237300021",
-  logo_url: "https://www.seekpng.com/png/detail/8-84419_linkedin-logo-png-icon-linkedin-logo-png.png",
-)
-linkedin.save!
-
- # emails
-require_relative 'emails'
-
-EMAILS.each do |mail|
-  update = Update.new(
-    api: Api.find_by_name(mail["api"]),
-    source: mail["from"],
-    title: mail["subject"],
-    text: mail["text"]
-    )
-  p update.save
-end
-
 # users
 alex = User.new(
     username: "alex"
@@ -92,6 +30,69 @@ todd = User.new(
     username: "todd"
   )
 todd.save!
+
+# APIS
+monday = Api.new(
+  name:   "Monday API",
+  status: "pending",
+  tray_version: "2.0.1",
+  latest_version: "2.0.1",
+  developer: alex,
+  description: "The monday.com API supports read and write (mutating) access to boards, items, and more. It gives you the ability to both view and use any data you have in monday.com outside of the platform. You can use the API to import external data into your monday.com account or trigger actions in monday.com in response to a change in an external software or service.",
+  api_acc_manager: "William Gates | team@monday.com | +447237300021",
+  logo_url: "https://4bo0cq4bxnou2rimh3hwzwi8-wpengine.netdna-ssl.com/blog/wp-content/uploads/2018/02/22852120_1266763086768693_6004893502123596052_n.png",
+)
+monday.save!
+
+microsoft = Api.new(
+  name:   "Microsoft 365 API",
+  status: "pending",
+  tray_version: "2.0.8",
+  latest_version: "2.0.11",
+  developer: todd,
+  description: "Office 365 services, such as OneNote, Outlook, Excel, OneDrive, Microsoft Teams, Planner, and SharePoint, are now exposed in Microsoft Graph. Microsoft Graph is a unified API endpoint for accessing data across Microsoft 365, which includes Office 365, Enterprise Mobility, and Security and Windows services. It provides a simplified developer experience, with one endpoint and a single authentication token that gives your app access to data across all these services.",
+  api_acc_manager: "Miranda Gates | conections@microsoft.com | +447237300021",
+  logo_url: "https://cdn1.iconfinder.com/data/icons/flat-and-simple-part-1/128/microsoft-512.png",
+)
+microsoft.save!
+
+oxford = Api.new(
+  name:   "Oxford Dictionaries API",
+  status: "pending",
+  tray_version: "2.7.6",
+  latest_version: "2.8.9",
+  developer: api,
+  description: "The Oxford Dictionaries API allows easy access to our world-renowned dictionary content. Use the live documentation below to try it out, view real responses, and explore a growing number of code samples to help you get started.",
+  api_acc_manager: "Colin Smith | emailresponses.ac@oup.com | +447237300021",
+  logo_url: "https://pbs.twimg.com/profile_images/875679902216970241/NAw23Gdg_400x400.jpg",
+)
+oxford.save!
+
+linkedin = Api.new(
+  name:   "LinkedIn API",
+  status: "pending",
+  tray_version: "1.5.4",
+  latest_version: "1.5.8",
+  developer: todd,
+  description: "LinkedIn's home for API documentation for all LinkedIn business lines. Our API documentation is organized by business lines covering Consumer, Compliance, Learning, Marketing, Sales, and Talent Solutions. Follow the links below to learn more about business lines and their possible integration types.",
+  api_acc_manager: "Rich Links | connections@microsoft.com | +447237300021",
+  logo_url: "https://www.seekpng.com/png/detail/8-84419_linkedin-logo-png-icon-linkedin-logo-png.png",
+)
+linkedin.save!
+
+ # emails
+require_relative 'emails'
+
+EMAILS.each do |mail|
+  update = Update.new(
+    api: Api.find_by_name(mail["api"]),
+    source: mail["from"],
+    title: mail["subject"],
+    text: mail["text"]
+    )
+  p update.save
+end
+
 
 # notes
 
